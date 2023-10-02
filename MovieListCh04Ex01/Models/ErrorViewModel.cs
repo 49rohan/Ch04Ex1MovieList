fbf9c89c19b38189a46bd191d49aba3c63430010
Rohan Basnet <rohan.basnet@southeasttech.edu>
2023-10-01 22:17:17 -0500

    Add project files.

---
+++ MovieListCh04Ex01/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MovieListCh04Ex01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
